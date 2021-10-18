68ad53476e4d6c11031f3bc0ab7b0955c9dafb46
xeelix <fritlangoof@gmail.com>
2021-10-18 17:35:22 +0700

    tailwind.config.js changes for Netlify build #3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-	purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+	purge: {
+		content: [
+			"./src/**/*.{js,jsx,ts,tsx}",
+			"./src/components/**/*.{js,jsx,ts,tsx}",
+			'public/**/*.html',
+		],
+	},
 	darkMode: false, // or 'media' or 'class'
 	theme: {
 		backgroundColor: (theme) => ({
